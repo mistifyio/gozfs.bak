a1c04b5046e905604a57ca7b6d22174b0b38aaf2
Manuel Mendez <mmendez534@gmail.com>
2015-09-17 13:29:33 -0400

    nv: add diff function to hightlight diffs in byte arrays

--- nv/encode_test.go
+++ nv/encode_test.go
@@ -1,9 +1,52 @@
 package nv
 
 import (
+	"fmt"
 	"reflect"
+	"strings"
 	"testing"
 )
+
+func diff(want, got []byte) (string, string) {
+	w := strings.Split(fmt.Sprintf("%03v", want), " ")
+	w[0] = strings.TrimLeft(w[0], "[")
+	w[len(w)-1] = strings.TrimRight(w[len(w)-1], "]")
+
+	g := strings.Split(fmt.Sprintf("%03v", got), " ")
+	g[0] = strings.TrimLeft(g[0], "[")
+	g[len(g)-1] = strings.TrimRight(g[len(g)-1], "]")
+
+	min := len(w)
+	if len(g) < min {
+		min = len(g)
+	}
+	red := "\x1b[31;1m"
+	green := "\x1b[32;1m"
+	reset := "\x1b[0m"
+	diff := false
+	for i := 0; i < min; i++ {
+		if g[i] != w[i] {
+			if diff == false {
+				diff = true
+				g[i] = red + g[i]
+				w[i] = green + w[i]
+			}
+		} else if diff {
+			diff = false
+			g[i] = reset + g[i]
+			w[i] = reset + w[i]
+		}
+	}
+	if diff {
+		g[len(g)-1] += reset
+		w[len(w)-1] += reset
+	}
+	w[0] = "[" + w[0]
+	w[len(w)-1] += "]"
+	g[0] = "[" + g[0]
+	g[len(g)-1] += "]"
+	return strings.Join(w, " "), strings.Join(g, " ")
+}
 
 func TestEncodeGood(t *testing.T) {
 	for _, s := range good {
@@ -16,7 +59,9 @@ func TestEncodeGood(t *testing.T) {
 			t.Fatal(s.name, err)
 		}
 		if !reflect.DeepEqual(b, s.payload) {
-			t.Fatalf("%s failed: encoded does not match payload\nwant:|%v|\n got:|%v|\n", s.name, s.payload, b)
+			want, got := diff(s.payload, b)
+			t.Fatalf("%s failed: encoded does not match payload\nwant:|%s|\n got:|%s|\n",
+				s.name, want, got)
 		}
 	}
 }
